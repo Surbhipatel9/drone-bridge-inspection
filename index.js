976d7335a9352c2135b68656113592f3f7f31573
natemiklas1 <natemiklas1@gmail.com>
2018-02-20 08:51:03 -0500

    added picture to home page correctly

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var http = require('http');
 var fs = require('fs');
 var express = require('express');
 var app = express();
-
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.set('views', 'public')
@@ -10,14 +9,8 @@ app.set('views', 'public')
 app.get('/', (req,res) => {
   res.render('index.ejs')
 })
+
 app.listen(8080)
-// http.createServer(function (req, res) {
-//   fs.readFile('index.ejs', function(err, data) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     res.end();
-//   });
-// }).listen(8080);
 
 
 /*
