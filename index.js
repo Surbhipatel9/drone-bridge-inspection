06b700c002a65fb0d48f4d321c9f6be13f3a2c80
natemiklas1 <natemiklas1@gmail.com>
2018-02-21 13:04:14 -0500

    update home page

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ app.get('/', (req,res) => {
   res.render('index.ejs')
 })
 
+/*
+function fillHomeTable(str){
+  var xhttp;
+  if(str == ""){
+    document.getElementById("txtHint").innerHTML = "";
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+      document.getElementById("txtHint").innerHTML = 
+      this.responseText;
+  }
+};
+xhttp.open("GET", "index.ejs?q=" + str, true);
+xhttp.send();
+}
+*/
 app.listen(8080)
 
 
