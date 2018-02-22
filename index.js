3bd3db640651d0d3ab6cb6e15359dc3e68c07481
David Sloan <sloan36@marshall.edu>
2018-02-21 22:18:05 -0500

    Login works but not pretty

--- index.js
+++ index.js
@@ -2,12 +2,69 @@ var http = require('http');
 var fs = require('fs');
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var db = require('./DB.js');
+const path = require('path');
+const LocalStrategy = require('passport-local').Strategy;
 app.use(express.static('public'))
+
+// Passport middleware.
+app.use(require('express-session')({
+	secret: 'dryooisacoolguy',
+	resave: false,
+	saveUninitialized: false
+}));
 app.set('view engine', 'ejs')
 app.set('views', 'public')
+app.use(cookieParser());
+// Body parser middleware.
+app.use(bodyParser.json());			// to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: true
+})); 
+
+// required for passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash()); // use connect-flash for flash messages stored in session
+require('./passport')(passport);
 
 app.get('/', (req,res) => {
   res.render('index.ejs')
+  //if(req.session.passport)
+    //console.log(req.session.passport.user)  //GET SESSION INFO FOR CURRENTLY LOGGED IN USER
+})
+
+// LOCAL LOGIN ROUTE
+app.get('/login', (req, res, passport) => {
+	res.render('login.ejs', {message: req.flash('loginMessage')});
+});
+
+//LOCAL LOGIN POST ROUTE
+//authenticate the login and redirect on success/failure and send failure message if needed
+app.post('/login', passport.authenticate('local-login', {
+	successRedirect: '/',
+	failureRedirect: '/login',
+	failureFlash: true
+}), (res, req) => {
+});
+
+app.get('/registration', (req,res,passport) => {
+  res.render('registration.ejs', {message:req.flash('loginMessage')})
+})
+
+app.post('/registration', passport.authenticate('local-signup', {
+  successRedirect:'/',
+  failureRedirect:'/registration',
+  failureFlash:true }), (req,res) => {})
+
+app.get('/logout', (req,res) => {
+  req.logout()
+  req.session.passport.user = false;
+  res.redirect('/');
 })
 
 /*
